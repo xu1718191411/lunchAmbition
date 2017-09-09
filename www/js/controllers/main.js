c4481c8fbb9f3eaa11cf6f5eba6cce9ec7a48cfb
Yusuke Nagase <aaa@aaa.com>
2017-09-09 15:32:09 +0900

    nagase

---
+++ www/js/controllers/main.js
@@ -0,0 +1,11 @@
+module.controller('mainCtrl', function ($scope) {
+    // ここにJavaScriptを利用した、動的なアプリの動作を書いていきます
+
+    $scope.moveToPage = function(page) {
+        if (page === "page1") {
+            pageNavigator.pushPage('page1.html');
+        }else if(page==='back'){
+            pageNavigator.popPage(); //1つ前の画面に戻る
+        }
+    }
+});
---
+++ www/js/start.js
@@ -0,0 +1,12 @@
+var module = angular.module('app', ['onsen']);
+angular.element(document).ready(function () {
+    if (window.cordova) {
+        document.addEventListener('deviceready', function () {
+            console.log("端末の準備ができました");
+            angular.bootstrap(document.body, ['app']);
+        }, false);
+    } else {
+        console.log("ブラウザの準備ができました");
+        angular.bootstrap(document.body, ['app']);
+    }
+});
