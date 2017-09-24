ab7270b433e39a3dcb5f4ddf53e13293bd3e7fb1
Rika Miyamoto <lika0817@gmail.com>
2017-09-24 18:25:31 +0900

    MyPage　と　TeamPageを作ってみました

---
+++ www/js/ChangeTab.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+<!--
+function ChangeTab(tabname) {
+   // 全部消す
+   document.getElementById('tab1').style.display = 'none';
+   document.getElementById('tab2').style.display = 'none';
+   // 指定箇所のみ表示
+   document.getElementById(tabname).style.display = 'block';
+}
+ -->
+</script>
