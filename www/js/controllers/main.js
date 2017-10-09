56135613d983387eae5adb6a5ba7432c4f7db3ab
xu1718191411 <xu1718191411@gmail.com>
2017-10-09 18:03:35 +0900

    finnaly I load the google map by angularJs1.0

--- www/js/controllers/main.js
+++ www/js/controllers/main.js
@@ -20,4 +20,10 @@ module
           mySplitter.left.close();
         });
     };
-  });
+});
+
+module.controller('MapController',function($scope){
+    $scope.name = "XU ZHONGWEI"
+
+    $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+})
--- www/js/start.js
+++ www/js/start.js
@@ -1,4 +1,4 @@
-var module = angular.module('app', ['onsen']);
+var module = angular.module('app', ['onsen','uiGmapgoogle-maps']);
 angular.element(document).ready(function () {
     if (window.cordova) {
         document.addEventListener('deviceready', function () {
@@ -10,3 +10,27 @@ angular.element(document).ready(function () {
         angular.bootstrap(document.body, ['app']);
     }
 });
+
+// module.config(['uiGmapGoogleMapApiProvider', function(uiGmapGoogleMapApiProvider) {
+//     uiGmapGoogleMapApiProvider.configure({
+//         key: 'AIzaSyAOvoVMLV8MsHdpyCCriNbY4Jzmeu2utCE',
+//         v: '3.17',
+//         libraries: 'weather,geometry,visualization'
+//     });
+// }]);
+module.config(
+    ['uiGmapGoogleMapApiProvider', function(GoogleMapApiProviders) {
+        GoogleMapApiProviders.configure({
+            china: true
+        });
+    }]
+);
+      
+
+// module.config(
+//     ['uiGmapGoogleMapApiProvider', function(GoogleMapApiProviders) {
+//         GoogleMapApiProviders.configure({
+//             china: true
+//         });
+//     }]
+// );
