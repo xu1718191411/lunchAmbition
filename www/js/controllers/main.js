db0e3f19d4583463f7dd2a74bd7de757114ded4c
syoui <syoui@iMac-3.local>
2017-11-13 12:05:55 +0900

    implements geocoding API

--- www/js/controllers/main.js
+++ www/js/controllers/main.js
@@ -31,14 +31,48 @@ module
            var vm = this;
             vm.types = "['establishment']";
             vm.placeChanged = function() {
-              vm.place = this.getPlace();
-              console.log('location', vm.place.geometry.location);
-              vm.map.setCenter(vm.place.geometry.location);
+              $scope.searchFromKeyWords(vm.address)
+              // vm.place = this.getPlace();
+              // if(vm.place == undefined) return;
+              // if(vm.place.geometry == undefined) return;
+              // console.log('location', vm.place.geometry.location);
+              // vm.map.setCenter(vm.place.geometry.location);
             }
 
             NgMap.getMap().then(function(map) {
               vm.map = map;
-            });
+              var geocoder = new google.maps.Geocoder();
+              vm.geocoder = geocoder;
 
+
+              $scope.searchFromKeyWords = function(text){
+
+                        vm.geocoder.geocode({'address': text}, function(results, status) {
+                          if (status === 'OK') {
+                            vm.map.setCenter(results[0].geometry.location);
+                            console.log("results",results);
+                            for(var i=0;i<results.length;i++){
+                                    var marker = new google.maps.Marker({
+                                      map: vm.map,
+                                      position: results[i].geometry.location
+                                    });
+                            }
+
+                          } else {
+                            alert('Geocode was not successful for the following reason: ' + status);
+                          }
+                        });         
+              }
+
+
+        });
 
     });
+
+
+
+
+
+
+
+
