26b435982094fde0a8809f649f19867404bcc959
xu1718191411 <xu1718191411@gmail.com>
2017-11-18 15:52:33 +0900

    myPage

--- www/js/controllers/myPage.js
+++ www/js/controllers/myPage.js
@@ -1,3 +1,26 @@
 module.controller('MyPageController',function($scope){
     $scope.name = "MIYAMOTO"
+
+	$scope.dataArray = [{
+        shopName: "aaa",
+        expense:  55,
+    }, {
+        shopName: "bbb",
+        expense: 60,
+    },
+    {
+        shopName: "bbb",
+        expense: 60,
+    },
+    {
+        shopName: "bbb",
+        expense: 60,
+    },
+    {
+        shopName: "bbb",
+        expense: 60,
+    }];
+
+
+
 })
