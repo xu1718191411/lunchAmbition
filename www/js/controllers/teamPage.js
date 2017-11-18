cc682bba0c21a37331d3742caf6efd91ee4eb3f3
Rika Miyamoto <lika0817@gmail.com>
2017-11-18 14:54:27 +0900

    test

--- www/js/controllers/teamPage.js
+++ www/js/controllers/teamPage.js
@@ -1,3 +1,18 @@
 module.controller('TeamController',function($scope){
     $scope.name = "MIYAMOTO"
+
+    $scope.dataArray = [{
+        value: "aaa",
+        title: "ttt1",
+        areaname: "name1"
+    }, {
+        value: "bbb",
+        title: "ttt2",
+        areaname: "name2"
+    }, {
+        value: "ccc",
+        title: "ttt3",
+        areaname: "name3"
+    }];
+
 })
