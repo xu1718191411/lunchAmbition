93ee8431cff28f2f1779aeb3052380b099f15a2f
長野　めぐみ <nagano.megumi@pt.com>
2017-11-18 18:22:08 +0900

    I am muzuno

--- www/js/controllers/list.js
+++ www/js/controllers/list.js
@@ -1,3 +1,31 @@
 module.controller('LISTController',function($scope){
     $scope.name = "NAGANO"
+    $scope.lunch = {};
+    $scope.lunch.list = [];
+    getShopList()
+
+
+    function getShopList(){
+        var lunchShop = ncmb.DataStore("ShopList");    //データテーブル作成
+        lunchShop.fetchAll()
+        .then(function(results){
+            $scope.apply(function(){
+                $scope.lunch.list = list;
+                console.log($scope.lunch.list);
+                $scope.name = "hahahahaha"
+            })
+        })
+        .catch(function(err){
+
+        });
+    };
+
+
+    // document.addEventListener('pageinit',function(e){
+    //     if(e.target.id === "listPage"){
+    //         getShopList();
+        
+    //     }
+    // });
+
 })
