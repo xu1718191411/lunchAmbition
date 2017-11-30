5da20b8da3b59310076089fedf93ffe167f1e486
Rika Miyamoto <lika0817@gmail.com>
2017-11-30 22:22:34 +0900

    team page update

--- www/js/controllers/teamPage.js
+++ www/js/controllers/teamPage.js
@@ -1,21 +1,27 @@
 module.controller('TeamController',function($scope){
     $scope.name = "MIYAMOTO"
+    $scope.lunch = {};
+    $scope.lunch.HighPointUser = [];
+    getHighPoint()
 
-    $scope.dataArray = [{
-        number: "1",
-        username: "User1",
-        userpoint: "900",
-        userlank: "lank1"
-    }, {
-        number: "2",
-        username: "User2",
-        userpoint: "800",
-        userlank: "lank2"
-    }, {
-        number: "3",
-        username: "User3",
-        userpoint: "700",
-        userlank: "lank3"
-    }];
+    function getHighPoint(){
+        var PointList = ncmb.DataStore("GetPointList");    
+        PointList.fetchAll()
+        .then(function(result){
+ 
+            $scope.dataArray(function(){
+                $scope.lunch.GetPoint=list;
+                console.log($scope.lunch.HighPointUser);
+                $scope.name ="No1";
+            })
 
-})
+        .catch(function(err){
+        });
+
+
+    /* document.addEventListener('pageinit',function(e){
+         if(e.target.id === "listPage"){
+             getHighPoint();
+            };
+          });*/
+
