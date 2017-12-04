02980261e11613148c72966a935058825e4195ea
syoui <syoui@iMac-3.local>
2017-12-04 18:20:40 +0900

    lunch data

--- www/js/controllers/teamPage.js
+++ www/js/controllers/teamPage.js
@@ -1,25 +1,24 @@
-module.controller('TeamController',function($scope){
+module.controller('TeamController', function($scope) {
     $scope.name = "MIYAMOTO"
     $scope.lunch = {};
     $scope.lunch.HighPointUser = [];
     getHighPoint()
 
-    function getHighPoint(){
-        var PointList = ncmb.DataStore("GetPointList");    
+    function getHighPoint() {
+        var PointList = ncmb.DataStore("GetPointList");
         PointList.fetchAll()
-        .then(function(result){
- 
-            $scope.dataArray(function(){
-                $scope.lunch.GetPoint=list;
-                console.log($scope.lunch.HighPointUser);
-                $scope.name ="No1";
-            })
-        })
-        .catch(function(err){
+            .then(function(result) {
+                console.log(111111111)
+                console.log(result);
+                $scope.$apply(function() {
+                    $scope.lunch.GetPoint = result;
 
-        });
+                    $scope.name = "No1";
+                })
+            })
+            .catch(function(err) {
+
+            });
 
     }
 })
-
-
